a38fb151581366de11625b88c62cb8472d0fbffb
Shubhi Jain <shubhij2@illinois.edu>
2023-04-23 14:32:13 -0500

    Added workload directory

--- benchmark/write_test.sh
+++ benchmark/write_test.sh
@@ -4,16 +4,19 @@
 sudo apt-get update
 sudo apt-get install parallel
 
+CLIENT_ID=$1
+
 clients=5
 
-command="mvn exec:java -Dexec.mainClass=io.skyrosforkafka.KafkaClient -Dexec.args='--c config.properties --o put --op w_all --c_id 1 --key_sep=, --parse_key=true --t hello-topic --i'"
+command="mvn exec:java -Dexec.mainClass=io.skyrosforkafka.KafkaClient -Dexec.args='--c config.properties --o put --op w_all --c_id ${CLIENT_ID} --key_sep=, --parse_key=true --t hello-topic --i '"
 
 export SHELL=$(type -p bash)
+export command=$command
 
 run_executable() {
     input_file=$1
     echo "Processing ${input_file}"
-     ${input_file} 
+    $command${input_file} 
 }
 
 export -f run_executable
